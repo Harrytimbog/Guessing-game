0c5fe0555e83439f1eb676ab67b7c5b9b468e649
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-12 16:03:58 +0100

    Writing Platform specific code with Platform API (flavor B)

--- components/ui/Title.android.js
+++ components/ui/Title.android.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
     // borderWidth: Platform.OS === "android" ? 2 : 0, works perfectly
-    borderWidth: Platform.select({ ios: 0, android: 2 }),
+    borderWidth: 2,
     borderColor: "white",
     padding: 12,
     maxWidth: "80%", // Referes to the container that hold this element
